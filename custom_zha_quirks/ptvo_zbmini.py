fd0cb3640545e2c9677de4f36ebe4434617949f1
denpamusic <denpa@denpa.pro>
2023-10-23 21:55:40 +0300

    Add quirks for V3.
    Based on PTVO 2023-09-21.

--- custom_zha_quirks/ptvo_zbmini.py
+++ custom_zha_quirks/ptvo_zbmini.py
@@ -23,6 +23,7 @@ from zigpy.zcl.clusters.general import (
     OnOff,
     OnOffConfiguration,
 )
+from zigpy.zcl.clusters.measurement import TemperatureMeasurement
 
 PTVO: Final = "PTVO"
 
@@ -53,7 +54,7 @@ class DeviceTemperatureCluster(LocalDataCluster, DeviceTemperature):
 
 
 class PtvoZbminiLightV1(CustomDevice):
-    """PTVO ZBMINI light version 2."""
+    """PTVO ZBMINI light version 1."""
 
     signature = {
         MODELS_INFO: ((PTVO, "ZBMINI"),),
@@ -224,6 +225,95 @@ class PtvoZbminiLightV2(CustomDevice):
                     AnalogInputCluster,
                     DeviceTemperatureCluster,
                 ],
+            },
+            242: {
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        }
+    }
+
+
+class PtvoZbminiLightV3(CustomDevice):
+    """PTVO ZBMINI light version 3."""
+
+    signature = {
+        MODELS_INFO: ((PTVO, "ZBMINI"),),
+        ENDPOINTS: {
+            # <SimpleDescriptor endpoint=1 profile=260 device_type=61438
+            # device_version=1
+            # input_clusters=[0, 7]
+            # output_clusters=[0, 18]>
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: PtvoDeviceType.GENERIC,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    OnOffConfiguration.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    MultistateInput.cluster_id,
+                ],
+            },
+            # <SimpleDescriptor endpoint=2 profile=260 device_type=61438
+            # device_version=1
+            # input_clusters=[6, 7]
+            # output_clusters=[6]>
+            2: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: PtvoDeviceType.GENERIC,
+                INPUT_CLUSTERS: [
+                    OnOff.cluster_id,
+                    OnOffConfiguration.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    OnOff.cluster_id,
+                ],
+            },
+            # <SimpleDescriptor endpoint=3 profile=260 device_type=61438
+            # device_version=1
+            # input_clusters=[1026]>
+            3: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: PtvoDeviceType.GENERIC,
+                INPUT_CLUSTERS: [TemperatureMeasurement.cluster_id],
+            },
+            # <SimpleDescriptor endpoint=3 profile=41440 device_type=97
+            # device_version=1
+            # output_clusters=[33]>
+            242: {
+                PROFILE_ID: 41440,
+                DEVICE_TYPE: 0x0061,
+                OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
+            },
+        },
+    }
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    OnOffConfiguration.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    MultistateInput.cluster_id,
+                ],
+            },
+            2: {
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_LIGHT,
+                INPUT_CLUSTERS: [
+                    OnOff.cluster_id,
+                    OnOffConfiguration.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    OnOff.cluster_id,
+                ],
+            },
+            3: {
+                DEVICE_TYPE: zha.DeviceType.TEMPERATURE_SENSOR,
+                INPUT_CLUSTERS: [TemperatureMeasurement.cluster_id],
             },
             242: {
                 OUTPUT_CLUSTERS: [GreenPowerProxy.cluster_id],
