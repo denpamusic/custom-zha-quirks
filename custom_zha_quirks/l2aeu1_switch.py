11b7dc9c250b2215e957d48d8092db2b5728d518
denpamusic <denpa@denpa.pro>
2026-02-12 15:07:14 +0300

    Import automation triggers.

--- custom_zha_quirks/l2aeu1_switch.py
+++ custom_zha_quirks/l2aeu1_switch.py
@@ -6,81 +6,15 @@ Adds operation_mode select entities and button event support.
 from zigpy.profiles import zha
 from zigpy.quirks.v2 import QuirkBuilder, EntityType
 
-from zhaquirks.const import (
-    ARGS,
-    ATTR_ID,
-    BUTTON,
-    BUTTON_1,
-    BUTTON_2,
-    CLUSTER_ID,
-    COMMAND,
-    COMMAND_BUTTON_DOUBLE,
-    COMMAND_BUTTON_HOLD,
-    COMMAND_BUTTON_SINGLE,
-    COMMAND_DOUBLE,
-    COMMAND_SINGLE,
-    ENDPOINT_ID,
-    PRESS_TYPE,
-    VALUE,
-)
+
 from zhaquirks.xiaomi import LUMI
 from zhaquirks.xiaomi.aqara.opple_switch import (
-    BOTH_BUTTONS,
     OppleOperationMode,
     OppleIndicatorLight,
     OppleSwitchCluster,
+    XiaomiOpple2ButtonSwitchBase,
 )
 from zhaquirks.xiaomi.aqara.opple_remote import MultistateInputCluster
-
-
-# Device automation trigger definitions matching XiaomiOpple2ButtonSwitchBase exactly
-DEVICE_AUTOMATION_TRIGGERS = {
-    # Button 1 (left) - endpoint 41
-    (COMMAND_BUTTON_SINGLE, BUTTON_1): {
-        ENDPOINT_ID: 41,
-        CLUSTER_ID: 18,
-        COMMAND: "41_single",
-        ARGS: {ATTR_ID: 0x0055, PRESS_TYPE: COMMAND_SINGLE, VALUE: 1},
-    },
-    (COMMAND_BUTTON_DOUBLE, BUTTON_1): {
-        ENDPOINT_ID: 41,
-        CLUSTER_ID: 18,
-        COMMAND: "41_double",
-        ARGS: {ATTR_ID: 0x0055, PRESS_TYPE: COMMAND_DOUBLE, VALUE: 2},
-    },
-    # Button 2 (right) - endpoint 42
-    (COMMAND_BUTTON_SINGLE, BUTTON_2): {
-        ENDPOINT_ID: 42,
-        CLUSTER_ID: 18,
-        COMMAND: "42_single",
-        ARGS: {ATTR_ID: 0x0055, PRESS_TYPE: COMMAND_SINGLE, VALUE: 1},
-    },
-    (COMMAND_BUTTON_DOUBLE, BUTTON_2): {
-        ENDPOINT_ID: 42,
-        CLUSTER_ID: 18,
-        COMMAND: "42_double",
-        ARGS: {ATTR_ID: 0x0055, PRESS_TYPE: COMMAND_DOUBLE, VALUE: 2},
-    },
-    # Both buttons - endpoint 51
-    (COMMAND_BUTTON_SINGLE, BOTH_BUTTONS): {
-        ENDPOINT_ID: 51,
-        CLUSTER_ID: 18,
-        COMMAND: "51_single",
-        ARGS: {ATTR_ID: 0x0055, PRESS_TYPE: COMMAND_SINGLE, VALUE: 1},
-    },
-    (COMMAND_BUTTON_DOUBLE, BOTH_BUTTONS): {
-        ENDPOINT_ID: 51,
-        CLUSTER_ID: 18,
-        COMMAND: "51_double",
-        ARGS: {ATTR_ID: 0x0055, PRESS_TYPE: COMMAND_DOUBLE, VALUE: 2},
-    },
-    # Hold events come from cluster 0xFCC0 on endpoint 1
-    (COMMAND_BUTTON_HOLD, BUTTON): {
-        ENDPOINT_ID: 1,
-        CLUSTER_ID: 0xFCC0,
-        ARGS: {ATTR_ID: 0x00FC, VALUE: 0},
-    },
-}
 
 (
     QuirkBuilder(LUMI, "lumi.switch.l2aeu1")
@@ -126,6 +60,6 @@ DEVICE_AUTOMATION_TRIGGERS = {
         fallback_name="Indicator Light Mode",
     )
     # Add device automation triggers for button events
-    .device_automation_triggers(DEVICE_AUTOMATION_TRIGGERS)
+    .device_automation_triggers(XiaomiOpple2ButtonSwitchBase.device_automation_triggers)
     .add_to_registry()
 )
